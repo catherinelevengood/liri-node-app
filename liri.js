0424bf8abd93b39155d129699ffc0fc0685fa691
Catherine Levengood <angel_avelanes@yahoo.com>
2018-10-10 23:46:49 -0400

    finish concert-this but still need format datetime with moment.js

--- liri.js
+++ liri.js
@@ -19,41 +19,59 @@ var concertThis = function(liriSearch) {
     
         // If there were no errors and the 	response code was 200 (i.e. the request was successful)...
         if (!error && response.statusCode === 200) {
-            
-            
-            
          //transform the body string into json object JSON.parse()    
         var jsonBody = JSON.parse(body)
 
+        for (var count = 0; count < jsonBody.length; count ++ ) {
+         console.log("Index of the array: " + count)
         //First OBject in the metalicca array
         //console.log(jsonBody[0]);
         // Venue Object inside the metallica array jsonBody[0]venue 
        // console.log(jsonBody[0].venue)
         // Acceessing the name variable inside of venue object
-        console.log("Concert Venue: " + jsonBody[0].venue.name)
+        console.log("Concert Venue: " + jsonBody[count].venue.name)
         // accessing the datetime variable inside of jsonBody
-        console.log("Concert Time: " + jsonBody[0].datetime)
+        console.log("Concert Time: " + jsonBody[count].datetime)
         //Access the City and region the venue is in
-        console.log("Concert Location: " + jsonBody[0].venue.city +", "+ jsonBody[0].venue.region)
+        console.log("Concert Location: " + jsonBody[count].venue.city +", "+ jsonBody[count].venue.region)
     
         }
-
+    }
         else{
             console.log(error);
         }
       });
     }
 
+    //TODO 1
+    var movieThis = function(liriSearch){
+
+
+    }
+
+    //TODO 2
+
+    var spotifyThis = function(liriSearch){
+
+    }
+
+
+
+    //TODO 3
+
+    var doWhatItSays = function(){
+
+    }
 
 switch (liriMethod) {
     case "concert-this":
          concertThis(liriSearch)
         break;
     case "spotify-this-song":
-         spotifyThis()
+         spotifyThis(liriSearch)
          break;
     case "movie-this":
-         movieThis()
+         movieThis(liriSearch)
          break;
     case "do-what-it-says":
          doWhatItSays()
